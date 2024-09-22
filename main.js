2c63bb5902ba42b70b7fb6ede6002c32175d8423
youfeed <11247005@qq.com>
2024-09-22 23:08:28 +0800

    add

--- main.js
+++ main.js
@@ -2,21 +2,23 @@ import './style.css'
 import plus from './lib/index.js'
 
 let PLUS = plus({
-  apikey:'pt0ALFviHU5yHTb0acTkLAF7Tg53BP8ld7F3pHqVMDBI233JVN0CykDFx9qZDxWteH1XxN0r__54h1dY_OzqhtRCFoIoLQWcyflHkarkp0o',
+  // apikey:'pt0ALFviHU5yHTb0acTkLAF7Tg53BP8ld7F3pHqVMDBI233JVN0CykDFx9qZDxWteH1XxN0r__54h1dY_OzqhtRCFoIoLQWcyflHkarkp0o',
+  apikey:'it-lk1ER3z-iVEDdHRtIxh-jwymtE65cfZwjh2OUOYbBg3E6Z54DILcVgfbi5KYyn5J0OVyCTReXCIaFtgGxdw',
   notify:'https://www.youloge.com/yaroslaff/ws-emit?site=125245'
 })
 console.log('PLUS',PLUS)
 // 调试单点登录
 document.querySelector('#sso').onclick = ()=>{
+  console.info(777777777,'sso.emit')
   PLUS.sso({
     // selector:null,
     selector:'#sso3',
-  }).listener(res=>{
-    console.info(res)
+  }).emit(res=>{
+    console.info(777777777,'sso.emit',res)
   }).then(res=>{
-    console.log(res)
+    console.log(777777777,'sso.then',res)
   }).catch(err=>{
-    console.log(err)
+    console.log(777777777,'sso.catch',err)
   })
 }
 document.querySelector('#captcha').onclick = ()=>{
@@ -51,13 +53,13 @@ document.querySelector('#hello').onclick = ()=>{
   console.log(login)
 
   login.emit(err=>{
-    console.log('listener',err)
+    console.log(888888888888888,'emit',err)
   })
   
   login.then(res=>{
-    console.log('res',res)
+    console.log(888888888888888,'then',res)
   }).catch(err=>{
-    console.log('err',err)
+    console.log(888888888888888,'catch',err)
   })
 
 
