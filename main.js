0ce0e60b7020978b1375d55f14a163702f3ba33e
youfeed <11247005@qq.com>
2024-09-22 15:35:05 +0800

    add

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import './style.css'
 import plus from './lib/index.js'
 
 let PLUS = plus({
-  apikey:'2UsDQYxLJOpdXVWRi4ujVAnIHZUNww92EYAeC6GsQs9q19ccNYDlnFrTe4PGIxsEBTFjjl4w8IFyDzQtQIWMWHpufbals5DrZHsJ3m0VFMMit4FAByb4X00/wqWS1kEy',
-  notify:'https://github.com/yaroslaff/ws-emit'
+  apikey:'pt0ALFviHU5yHTb0acTkLAF7Tg53BP8ld7F3pHqVMDBI233JVN0CykDFx9qZDxWteH1XxN0r__54h1dY_OzqhtRCFoIoLQWcyflHkarkp0o',
+  notify:'https://www.youloge.com/yaroslaff/ws-emit?site=125245'
 })
 console.log('PLUS',PLUS)
 // 调试单点登录
@@ -19,22 +19,26 @@ document.querySelector('#sso').onclick = ()=>{
     console.log(err)
   })
 }
-// document.querySelector('#captcha').onclick = ()=>{
-//   let captcha = PLUS.captcha({
-//     width:'360'
-//   }).then(res=>{
-//     console.log('#captcha',res)
-//   })
-//   .catch(err=>{
-//     console.log('#captcha',err)
-//   })
-//   console.log(captcha)
-// }
+document.querySelector('#captcha').onclick = ()=>{
+  const captcha = PLUS.captcha({
+    width:'360'
+  });
+  captcha.emit(res=>{
+    console.log('#captcha.emit',res)
+  })
+  
+  captcha.then(res=>{
+    console.log('#captcha.then',res)
+  }).catch(err=>{
+    console.log('#captcha.catch',err)
+  })
+  console.log(captcha)
+}
 
 document.querySelector('#hello').onclick = ()=>{
 
   // let ppp = plus({ukey:'TKoLtLJatVyqbbNWQFb_yMdoFzoWx40b9I7JzUYwRORqiHB7MxNdfqpN8hnSsx3hdbThUbauq0M60DNkZQZDrQ=='})
-  let login = PLUS.login({width:'360',close:true,selector:'#ss'})
+  let login = PLUS.login({width:'360',close:true,selector:0})
   // let ssos = ppp.sso({width:'360'})
   // console.log(plus,ppp,sso)
   // let P = plus.sso({
